d9c6b559021580cc989f3a48d3be07257f3683a0
yangyatian <yangyatian@163.com>
2022-04-10 15:52:14 +0800

    test2.py

--- test2.py
+++ test2.py
@@ -5,8 +5,12 @@ class Test(object):
         for i in num:
             if i == 'a':
                 print("这里包括a")
+	def test3(self,a=1,b=2):
+		c = a * b
+		print(c)
 
 
 if __name__ == "__main__":
     Test().test1()
     Test().test2()
+	Test().test3()
